525a8a88db053aae041153eea8fd9e1022f8ca0d
Joseph Farrish <joseph_a_f@hotmail.com>
2020-08-05 21:05:22 -0700

    generating random values but the problem is you have pattern which takes away from the random nature we seek

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
+
 // Assignment Code
-
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//functions
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  console.log(password);
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+//used to check an integer
+function isNumber(testNum) {
+  if (typeof testNum === 'number' && testNum >= 8 && testNum <= 128) {
+    return true;
+  } else { return false; }
+}
+//used to verify bool
+function isBool(testBool) {
+  if (typeof (testBool) === "boolean")
+    return true;
+}
 
 function generatePassword() {
   var lcArray = "abcdefghijklmnopqrstuvwxyz"; //lc = lower case array
@@ -23,7 +34,7 @@ function generatePassword() {
   var cLength 
   for(lengthChk = false; lengthChk === false;) //loop if parametrs are invalid.
   {
-      cLength = parseInt(prompt("How many charcters do you want in your password? Please chose between 8 and 128."));   //int
+      cLength = parseInt(prompt("How many charcters do you want in your password? Please chose between 8 and 128."));   //cLength = character length.
       lengthChk = isNumber(cLength); //test cLength is correct to move on.
       if(lengthChk === false) ("invalid selection try again"); 
   }
@@ -40,7 +51,7 @@ function generatePassword() {
   var numeric = confirm("Do you want numerical characters?");         //bool
   var specialChr = confirm("Do you want special characters?")         //bool
   
-   if((alpha && numeric && special) === false)
+   if((alpha && numeric && specialChr) === false)
    {
       alert("Your selections dosen't allow for a valid number randomization, re-run script.");
       return; 
@@ -52,35 +63,36 @@ function generatePassword() {
   console.log(upperCase);
   console.log(numeric);
   console.log(specialChr);
-   
-  //**make sure one of the character types are selected */
-  //generate the password
-  //return value.
-  
- 
-  var text = prompt("enter text");
-  console.log(text);
-  return text;
-
-}
 
 
-function isNumber(testNum)
-{
-  if(typeof testNum === 'number' && testNum >= 8 && testNum <= 128 )
+  var rgnValue = "";   //rgn = random generated value holder
+
+  for(var i = 0; i < cLength; i++)
   {
-    return true;
-  } else{ return false;}
+    if(alpha && lowerCase){
+      rgnValue += genRandomValue(lcArray);
+    }
+
+    if(alpha && upperCase){
+      rgnValue += genRandomValue(ucArray);
+    }
+    if(numeric){
+      rgnValue += genRandomValue(num);
+    }
+
+    if(specialChr){
+      rgnValue += genRandomValue(spChar);
+    }
+  }
+  console.log(rgnValue);
+  return rgnValue;
+
 }
 
+function genRandomValue(aValue){
+   return aValue[Math.floor(Math.random() * aValue.length)];
+  }
 
-function isBool(testBool)
-{
-  if(typeof(testBool)=== "boolean")
-  return true;
-}
 
-function actualNumGen(/*pass obj that holds all the values*/)
-{
-  
-}
+
+
